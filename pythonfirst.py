032f2688094eae776bb937d9a11b4d78aae34db6
ssaalwa <salwabasheer977@gmail.com>
2024-10-13 14:04:04 +0400

    Add files via upload

---
+++ pythonfirst.py
@@ -0,0 +1,23 @@
+print("Hello,World!")
+print(5+3)
+print("Python","is","fun!")
+print("she said,\"Hello!\"")
+print("First line\nSecond line")
+print("Item 1\tItem 2\tItem 3")
+print("The result is", 42)
+print(12345)
+print(3.14159)
+print("Dollar sign: $")
+print("apple","banana","cherry",sep=",")
+print("Hello,",end="")
+print("World!")
+print("python!"*3)
+print("\u2764") # print a heart symbol
+print()
+print("Hello","World!")
+print(True)
+print(False)
+print(1.2e3) # Prints 1200.0
+print("""This is a multiline
+print statement.""")
+print("This is a blacklash:\\")
